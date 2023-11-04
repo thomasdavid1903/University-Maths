a94afb125e40fe36540ba823fc8954bf63e9cfde
tommy <thomasdavid190303@gmail.com>
2023-11-04 17:40:35 +0000

    updated

--- p-1 method.py
+++ p-1 method.py
@@ -2,17 +2,17 @@ import math
 
 
 def pollard_p_1(N, B):
-    a = 2
+    a = 2  # Start with a base value of 2
 
     for j in range(2, B + 1):
         a = pow(a, j, N)  # Compute a^j modulo N
 
-        p = math.gcd(a - 1, N)
+        p = math.gcd(a - 1, N)  # Calculate the GCD of (a^j - 1) and N
 
-        if p != 1 and p != N:
+        if p != 1 and p != N:  # Check if p is a non-trivial factor
             return f"{p} is a factor of {N}"
 
-    return "No result"
+    return "No result"  # If no factor found within the specified range
 
 
 # Example usage:
